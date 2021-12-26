20f5645fc51c9221a20020fc4f0d87ea4d22f6f1
Matt Lewis <matt@mplewis.com>
2021-12-25 21:01:48 -0700

    wip

--- main.go
+++ main.go
@@ -1,18 +1,31 @@
 package main
 
 import (
-	"github.com/mplewis/bluetooth"
+	"fmt"
+
+	"github.com/mplewis/bluecat/bluetooth"
 )
 
 var adapter = bluetooth.DefaultAdapter
 
+func parse(uuid4 string) bluetooth.UUID {
+	uuid128 := fmt.Sprintf("0000%s-0000-1000-8000-00805f9b34fb", uuid4)
+	uuid, err := bluetooth.ParseUUID(uuid128)
+	if err != nil {
+		panic(err)
+	}
+	return uuid
+}
+
 func main() {
-	// Enable BLE interface.
 	must("enable BLE stack", adapter.Enable())
 
-	// Start scanning.
+	uuids := []bluetooth.UUID{
+		parse("af30"),
+	}
+
 	println("scanning...")
-	err := adapter.Scan(func(adapter *bluetooth.Adapter, device bluetooth.ScanResult) {
+	err := adapter.Scan(uuids, func(adapter *bluetooth.Adapter, device bluetooth.ScanResult) {
 		println("found device:", device.Address.String(), device.RSSI, device.LocalName())
 	})
 	must("start scan", err)
