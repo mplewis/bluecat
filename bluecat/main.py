5c5c487172e15dee021a006328967cd4642feb81
Matt Lewis <matt@mplewis.com>
2021-12-27 02:44:45 -0700

    wip: pseudocode for python server

--- bluecat/main.py
+++ bluecat/main.py
@@ -1,9 +1,65 @@
+from queue import Queue
+
 from fastapi import FastAPI, File, UploadFile
 
 
+IDLE_TIMEOUT = 15  # seconds
+PRINTER_NAMES = [
+    "GT01",
+    "GB01",
+    "GB02",
+    "GB03",
+]
+
+
 app = FastAPI()
+print_queue = Queue()
+
+
+class Printer:
+    def __init__(self):
+        self.device = None
+
+    def connect(self):
+        if self.device:
+            raise DeviceStateError('Device is already connected')
+        self.device = scan_for(PRINTER_NAMES)
+
+    def disconnect(self):
+        if not self.device:
+            raise DeviceStateError('Device is not connected')
+        self.device.disconnect()
+        self.device = None
+
+    def send(self, cmds):
+        if not self.device:
+            raise DeviceStateError('Device is not connected')
+        # TODO
+        self.device.send(cmds)
+
+    def feed(self, lines: int):
+        cmds = build_commands_for('feed', lines)
+        self.send(cmds)
+
+    def print(self, image: bytes):
+        cmds = build_commands_for(image)
+        self.send(cmds)
+
+
+def wait_for_jobs():
+    while True:
+        job = print_queue.get(timeout=IDLE_TIMEOUT)
+        if job:
+            send_to_printer(job)
+        else:
+            if connected:
+                disconnect()
 
 
 @app.post("/print")
 async def print(image: UploadFile = File(...)):
     return {"filename": image.filename}
+
+
+class DeviceStateError(Exception):
+    pass
