5d38fcb04847c1243d61e871e6c9e3678202a54a
Matt Lewis <matt@mplewis.com>
2021-12-27 00:56:15 -0700

    wip

---
+++ bt.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"fmt"
+
+	"tinygo.org/x/bluetooth"
+)
+
+func withConn(names []string, cb func(*bluetooth.Device, error)) {
+	adapter := bluetooth.DefaultAdapter
+	err := adapter.Enable()
+	if err != nil {
+		cb(nil, err)
+		return
+	}
+
+	ch := make(chan bluetooth.ScanResult)
+	go func() {
+		err = adapter.Scan(func(_ *bluetooth.Adapter, device bluetooth.ScanResult) {
+			for _, name := range names {
+				if device.LocalName() == name {
+					ch <- device
+					close(ch)
+					return
+				}
+			}
+		})
+		if err != nil {
+			cb(nil, err)
+		}
+	}()
+
+	// TODO: timeout
+	result := <-ch
+
+	device, err := adapter.Connect(result.Address, bluetooth.ConnectionParams{})
+	defer func() {
+		err = device.Disconnect()
+		if err != nil {
+			fmt.Println(err)
+		}
+	}()
+
+	cb(device, err)
+}
