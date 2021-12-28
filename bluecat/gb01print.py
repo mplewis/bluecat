c4b2665b6ee78279b2076cee7bbcf916eaf815a5
Matt Lewis <matt@mplewis.com>
2021-12-28 15:15:39 -0700

    refactor: reshape code more with enums

--- bluecat/gb01print.py
+++ bluecat/gb01print.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 import asyncio
 import argparse
+from typing import Union, Iterable
 
 from bleak import BleakClient, BleakScanner
 from bleak.exc import BleakError
@@ -10,20 +11,13 @@ import PIL.Image
 
 
 def main():
-    # General message format:
-    # Magic number: 2 bytes 0x51, 0x78
-    # Command: 1 byte
-    # 0x00
-    # Data length: 1 byte
-    # 0x00
-    # Data: Data Length bytes
-    # CRC8 of Data: 1 byte
-    # 0xFF
-    def format_message(command, data):
+    def format_message(command, data: Union[int, Iterable[int]]) -> bytes:
+        if not isinstance(data, Iterable):
+            data = [data]
         c = crc8()
         c.update(bytes(data))
         checksum = c.digest()[0]
-        data = (
+        return (
             [0x51, 0x78]
             + [command]
             + [0x00]
@@ -33,36 +27,37 @@ def main():
             + [checksum]
             + [0xFF]
         )
-        return data
 
     def printer_short(i):
         return [i & 0xFF, (i >> 8) & 0xFF]
 
-    # Commands
-    FeedPaper = 0xA1  # Data: Number of steps to go forward
-    DrawBitmap = (
-        0xA2  # Data: Line to draw. 0 bit -> don't draw pixel, 1 bit -> draw pixel
-    )
-    GetDevState = 0xA3  # Data: 0
-    ControlLattice = 0xA6  # Data: Eleven bytes, all constants. One set used before printing, one after.
-    OtherFeedPaper = (
-        0xBD  # Data: one byte, set to a device-specific "Speed" value before printing
-    )
-    #                              and to 0x19 before feeding blank paper
-    DrawingMode = 0xBE  # Data: 1 for Text, 0 for Images
-    SetEnergy = 0xAF  # Data: 1 - 0xFFFF
-    SetQuality = 0xA4  # Data: 0x31 - 0x35. APK always sets 0x33 for GB01
+    class Command:
+        FeedPaper = 0xA1  # steps to advance paper
+        DrawBitmap = 0xA2  # 1 bit = black dot
+        FeedRate = 0xBD
+        DrawingMode = 0xBE
+        SetEnergy = 0xAF  # 0x0001 to 0xFFFF
+        SetQuality = 0xA4
+        SetControlLattice = 0xA6  # 11-byte magic data
 
+    class FeedRate:
+        Print = 0x23
+        Blank = 0x19
+
+    class DrawingMode:
+        Image = 0x00
+        Text = 0x01
+
+    class EnergyMode:
+        Low = printer_short(8000)
+        Medium = printer_short(12000)
+        High = printer_short(17500)
+
+    PrintQuality = 0x33  # 0x31 to 0x35; app always uses 0x33
     PrintLattice = [0xAA, 0x55, 0x17, 0x38, 0x44, 0x5F, 0x5F, 0x5F, 0x44, 0x38, 0x2C]
     FinishLattice = [0xAA, 0x55, 0x17, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x17]
 
-    energy = {0: printer_short(8000), 1: printer_short(12000), 2: printer_short(17500)}
-    contrast = 1
-
     PrinterWidth = 384
-
-    ImgPrintSpeed = [0x23]
-    BlankSpeed = [0x19]
 
     feed_lines = 112
     header_lines = 0
@@ -101,17 +96,14 @@ def main():
                 if throttle is not None:
                     await asyncio.sleep(throttle)
 
-    def request_status():
-        return format_message(GetDevState, [0x00])
-
     def blank_paper(lines):
         # Feed extra paper for image to be visible
-        blank_commands = format_message(OtherFeedPaper, BlankSpeed)
+        blank_commands = format_message(Command.FeedRate, FeedRate.Blank)
         count = lines
         while count:
             feed = min(count, 0xFF)
             blank_commands = blank_commands + format_message(
-                FeedPaper, printer_short(feed)
+                Command.FeedPaper, printer_short(feed)
             )
             count = count - feed
         return blank_commands
@@ -121,16 +113,11 @@ def main():
         nonlocal feed_lines
 
         cmdqueue = []
-        # Set quality to standard
-        cmdqueue += format_message(SetQuality, [0x33])
-        # start and/or set up the lattice, whatever that is
-        cmdqueue += format_message(ControlLattice, PrintLattice)
-        # Set energy used
-        cmdqueue += format_message(SetEnergy, energy[contrast])
-        # Set mode to image mode
-        cmdqueue += format_message(DrawingMode, [0])
-        # not entirely sure what this does
-        cmdqueue += format_message(OtherFeedPaper, ImgPrintSpeed)
+        cmdqueue += format_message(Command.SetQuality, PrintQuality)
+        cmdqueue += format_message(Command.SetControlLattice, PrintLattice)
+        cmdqueue += format_message(Command.SetEnergy, EnergyMode.High)
+        cmdqueue += format_message(Command.DrawingMode, DrawingMode.Image)
+        cmdqueue += format_message(Command.FeedRate, FeedRate.Print)
 
         if img.width > PrinterWidth:
             # image is wider than printer resolution; scale it down proportionately
@@ -177,10 +164,10 @@ def main():
 
                 bit += 1
 
-            cmdqueue += format_message(DrawBitmap, bmp)
+            cmdqueue += format_message(Command.DrawBitmap, bmp)
 
         # finish the lattice, whatever that means
-        cmdqueue += format_message(ControlLattice, FinishLattice)
+        cmdqueue += format_message(Command.SetControlLattice, FinishLattice)
 
         return cmdqueue
 
@@ -189,19 +176,7 @@ def main():
     )
     name_args = parser.add_mutually_exclusive_group(required=True)
     name_args.add_argument("filename", nargs="?", help="file name of an image to print")
-    name_args.add_argument(
-        "-e",
-        "--eject",
-        help="don't print an image, just feed some blank paper",
-        action="store_true",
-    )
     feed_args = parser.add_mutually_exclusive_group()
-    feed_args.add_argument(
-        "-E",
-        "--no-eject",
-        help="don't feed blank paper after printing the image",
-        action="store_true",
-    )
     feed_args.add_argument(
         "-f",
         "--feed",
@@ -229,14 +204,6 @@ def main():
         action="store_const",
         dest="contrast",
         const=0,
-    )
-    contrast_args.add_argument(
-        "-m",
-        "--medium",
-        help="use moderate energy for moderate contrast",
-        action="store_const",
-        dest="contrast",
-        const=1,
     )
     contrast_args.add_argument(
         "-d",
@@ -285,22 +252,19 @@ def main():
         help="length of a command queue packet (default: {})".format(packet_length),
     )
     args = parser.parse_args()
-    if args.contrast:
-        contrast = args.contrast
     throttle = args.throttle
     packet_length = args.packetsize
     feed_lines = args.feed
     if args.scale_feed:
         scale_feed = True
 
-    print_data = request_status()
-    if not args.eject:
-        image = PIL.Image.open(args.filename)
-        print_data = print_data + render_image(image)
-    if not args.no_eject:
-        print_data = print_data + blank_paper(feed_lines)
-    loop = asyncio.get_event_loop()
-    loop.run_until_complete(connect_and_send(print_data))
+    image = PIL.Image.open(args.filename)
+
+    print_data = []
+    print_data = print_data + render_image(image)
+    print_data = print_data + blank_paper(feed_lines)
+
+    asyncio.run(connect_and_send(print_data))
 
 
 if __name__ == "__main__":
