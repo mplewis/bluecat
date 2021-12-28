3109309c2db3be6b6a3f18845215c0d78e1a43e3
Matt Lewis <matt@mplewis.com>
2021-12-28 00:07:49 -0700

    feat: basic paper feed

--- bluecat/main.py
+++ bluecat/main.py
@@ -1,4 +1,6 @@
 import asyncio
+from enum import Enum
+from io import BytesIO
 from queue import Queue
 from time import time
 from typing import List, Optional
@@ -6,9 +8,20 @@ from typing import List, Optional
 from bleak import BleakClient, BleakScanner
 from bleak.backends.device import BLEDevice
 from bleak.backends.scanner import AdvertisementData
+from crc8 import crc8
 from fastapi import FastAPI, File, UploadFile
 
 
+Cmd = bytes
+Cmds = List[Cmd]
+
+
+CHR_PRINT = "0000ae01-0000-1000-8000-00805f9b34fb"
+CHR_NOTIFY = "0000ae02-0000-1000-8000-00805f9b34fb"
+
+CMD_FEED_PAPER = 0xA1
+
+DELAY_CMDS = 0.01  # seconds
 IDLE_TIMEOUT = 15  # seconds
 PRINTER_NAMES = [
     "GT01",
@@ -44,6 +57,25 @@ async def scan_for(names: List[str]) -> Optional[BLEDevice]:
     return device
 
 
+def checksum(data: bytes) -> int:
+    c = crc8()
+    c.update(data)
+    return c.digest()[0]
+
+
+def build_cmd(cmd: int, data: List[int]) -> Cmd:
+    return [
+        0x51,
+        0x78,
+        cmd,
+        0x00,
+        len(data),
+        0x00,
+        *data,
+        checksum(bytes(data)),
+        0xFF,
+    ]
+
 
 class Printer:
     def __init__(self):
@@ -51,41 +83,50 @@ class Printer:
         self.client = None
 
     def on_disconnect(self, _client):
-        print('Disconnected')
+        print("Disconnected")
         self.device = None
         self.client = None
 
     def on_notify(self, sender, data):
-        print(f'Notify: {sender}: {data}')
+        print(f"Notify: {sender}: {data}")
 
     async def connect(self):
         if self.device:
-            raise DeviceStateError('Device is already connected')
+            raise DeviceStateError("Device is already connected")
         self.device = await scan_for(PRINTER_NAMES)
         if not self.device:
-            raise DeviceStateError('Device not found')
+            raise DeviceStateError("Device not found")
         self.client = BleakClient(self.device, disconnected_callback=self.on_disconnect)
         await self.client.connect()
-        await self.client.start_notify('0000ae02-0000-1000-8000-00805f9b34fb', self.on_notify)
+        await self.client.start_notify(
+            "0000ae02-0000-1000-8000-00805f9b34fb", self.on_notify
+        )
 
     async def disconnect(self):
         if not self.device:
-            raise DeviceStateError('Device is not connected')
+            raise DeviceStateError("Device is not connected")
         await self.client.disconnect()
 
-    def send(self, cmds):
+    async def send(self, cmds: Cmds):
         if not self.device:
-            raise DeviceStateError('Device is not connected')
-        # TODO
-        self.device.send(cmds)
+            raise DeviceStateError("Device is not connected")
+        for cmd in cmds:
+            print(cmd)
+            await self.client.write_gatt_char(CHR_PRINT, cmd)
+            await asyncio.sleep(DELAY_CMDS)
 
-    def feed(self, lines: int):
-        cmds = build_commands_for('feed', lines)
-        self.send(cmds)
+    async def feed(self, lines: int):
+        cmds: Cmds = []
+        while lines > 0:
+            consumed = min(lines, 255)
+            cmds.append(build_cmd(CMD_FEED_PAPER, [consumed]))
+            lines -= consumed
+        print(repr(cmds))
+        await self.send(cmds)
 
-    def print(self, image: bytes):
-        cmds = build_commands_for(image)
-        self.send(cmds)
+    async def print(self, image: bytes):
+        cmds: Cmds = []
+        await self.send(cmds)
 
 
 def wait_for_jobs():
@@ -121,9 +162,11 @@ async def main():
     p = Printer()
     try:
         await p.connect()
-        print('OK')
+        print("OK")
+        await p.feed(1000)
     finally:
         await p.disconnect()
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     asyncio.run(main())
