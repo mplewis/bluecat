a9d80c57202f3ff5099956dc956970bee64e7749
Matt Lewis <matt@mplewis.com>
2021-12-28 17:53:44 -0700

    refactor: extract most body

--- bluecat/gb01print.py
+++ bluecat/gb01print.py
@@ -24,66 +24,129 @@ PRINTER_NAMES = [
 ]
 
 
+def printer_short(i):
+    return [i & 0xFF, (i >> 8) & 0xFF]
+
+
+class Command:
+    FeedPaper = 0xA1  # steps to advance paper
+    DrawBitmap = 0xA2  # 1 bit = black dot
+    SetFeedRate = 0xBD
+    SetDrawingMode = 0xBE
+    SetEnergy = 0xAF  # 0x0001 to 0xFFFF
+    SetQuality = 0xA4
+    SetControlLattice = 0xA6  # 11-byte magic data
+
+
+class FeedRate:
+    Print = 0x23
+    Blank = 0x19
+
+
+class DrawingMode:
+    Image = 0x00
+    Text = 0x01
+
+
+class EnergyMode:
+    Low = printer_short(8000)
+    Medium = printer_short(12000)
+    High = printer_short(17500)
+
+
+class PrintQuality:
+    A = 0x31
+    B = 0x32
+    C = 0x33  # Android app default
+    D = 0x34
+    E = 0x35
+
+
+class Lattice:
+    Start = [0xAA, 0x55, 0x17, 0x38, 0x44, 0x5F, 0x5F, 0x5F, 0x44, 0x38, 0x2C]
+    Finish = [0xAA, 0x55, 0x17, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x17]
+
+
+def format_message(command, data: Union[int, Iterable[int]]) -> bytes:
+    if not isinstance(data, Iterable):
+        data = [data]
+    c = crc8()
+    c.update(bytes(data))
+    checksum = c.digest()[0]
+    return (
+        [0x51, 0x78]
+        + [command]
+        + [0x00]
+        + [len(data)]
+        + [0x00]
+        + data
+        + [checksum]
+        + [0xFF]
+    )
+
+
+def cmd_print_image(img: PIL.Image) -> Iterable[int]:
+    cmds = []
+    cmds += format_message(Command.SetQuality, PrintQuality.C)
+    cmds += format_message(Command.SetControlLattice, Lattice.Start)
+    cmds += format_message(Command.SetEnergy, EnergyMode.High)
+    cmds += format_message(Command.SetDrawingMode, DrawingMode.Image)
+    cmds += format_message(Command.SetFeedRate, FeedRate.Print)
+
+    if img.width > PRINTER_WIDTH:
+        # image is wider than printer resolution; scale it down proportionately
+        scale = PRINTER_WIDTH / img.width
+        img = img.resize((PRINTER_WIDTH, int(img.height * scale)))
+    if img.width < (PRINTER_WIDTH // 2):
+        # scale up to largest whole multiple
+        scale = PRINTER_WIDTH // img.width
+        img = img.resize(
+            (img.width * scale, img.height * scale), resample=PIL.Image.NEAREST
+        )
+    img = img.convert("RGB").convert("1")
+    if img.width < PRINTER_WIDTH:
+        pad_amount = (PRINTER_WIDTH - img.width) // 2
+        padded_image = PIL.Image.new("1", (PRINTER_WIDTH, img.height), 1)
+        padded_image.paste(img, box=(pad_amount, 0))
+        img = padded_image
+
+    for y in range(0, img.height):
+        bmp = []
+        bit = 0
+
+        # pack image data into 8 pixels per byte
+        for x in range(0, img.width):
+            if bit % 8 == 0:
+                bmp += [0x00]
+            bmp[int(bit / 8)] >>= 1
+            if not img.getpixel((x, y)):
+                bmp[int(bit / 8)] |= 0x80
+            else:
+                bmp[int(bit / 8)] |= 0
+            bit += 1
+
+        cmds += format_message(Command.DrawBitmap, bmp)
+
+    cmds += format_message(Command.SetControlLattice, Lattice.Finish)
+    return cmds
+
+
+def cmd_feed_paper(lines):
+    blank_commands = format_message(Command.SetFeedRate, FeedRate.Blank)
+    while lines:
+        feed = min(lines, 0xFF)
+        blank_commands += format_message(Command.FeedPaper, printer_short(feed))
+        lines = lines - feed
+    return blank_commands
+
+
 @dataclass
 class Args:
     filename: str
 
 
 def main(args: Args):
-    def format_message(command, data: Union[int, Iterable[int]]) -> bytes:
-        if not isinstance(data, Iterable):
-            data = [data]
-        c = crc8()
-        c.update(bytes(data))
-        checksum = c.digest()[0]
-        return (
-            [0x51, 0x78]
-            + [command]
-            + [0x00]
-            + [len(data)]
-            + [0x00]
-            + data
-            + [checksum]
-            + [0xFF]
-        )
-
-    def printer_short(i):
-        return [i & 0xFF, (i >> 8) & 0xFF]
-
-    class Command:
-        FeedPaper = 0xA1  # steps to advance paper
-        DrawBitmap = 0xA2  # 1 bit = black dot
-        SetFeedRate = 0xBD
-        SetDrawingMode = 0xBE
-        SetEnergy = 0xAF  # 0x0001 to 0xFFFF
-        SetQuality = 0xA4
-        SetControlLattice = 0xA6  # 11-byte magic data
-
-    class FeedRate:
-        Print = 0x23
-        Blank = 0x19
-
-    class DrawingMode:
-        Image = 0x00
-        Text = 0x01
-
-    class EnergyMode:
-        Low = printer_short(8000)
-        Medium = printer_short(12000)
-        High = printer_short(17500)
-
-    class PrintQuality:
-        A = 0x31
-        B = 0x32
-        C = 0x33  # Android app default
-        D = 0x34
-        E = 0x35
-
-    PrintLattice = [0xAA, 0x55, 0x17, 0x38, 0x44, 0x5F, 0x5F, 0x5F, 0x44, 0x38, 0x2C]
-    FinishLattice = [0xAA, 0x55, 0x17, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x17]
-
     feed_lines = 112
-
     device = None
 
     def detect_printer(detected, _advertisement_data):
@@ -112,66 +175,11 @@ def main(args: Args):
                 data = data[CMD_MAX_LEN:]
                 await asyncio.sleep(CMD_DELAY)
 
-    def blank_paper(lines):
-        blank_commands = format_message(Command.SetFeedRate, FeedRate.Blank)
-        while lines:
-            feed = min(lines, 0xFF)
-            blank_commands += format_message(Command.FeedPaper, printer_short(feed))
-            lines = lines - feed
-        return blank_commands
-
-    def render_image(img):
-        nonlocal feed_lines
-
-        cmdqueue = []
-        cmdqueue += format_message(Command.SetQuality, PrintQuality.C)
-        cmdqueue += format_message(Command.SetControlLattice, PrintLattice)
-        cmdqueue += format_message(Command.SetEnergy, EnergyMode.High)
-        cmdqueue += format_message(Command.SetDrawingMode, DrawingMode.Image)
-        cmdqueue += format_message(Command.SetFeedRate, FeedRate.Print)
-
-        if img.width > PRINTER_WIDTH:
-            # image is wider than printer resolution; scale it down proportionately
-            scale = PRINTER_WIDTH / img.width
-            img = img.resize((PRINTER_WIDTH, int(img.height * scale)))
-        if img.width < (PRINTER_WIDTH // 2):
-            # scale up to largest whole multiple
-            scale = PRINTER_WIDTH // img.width
-            img = img.resize(
-                (img.width * scale, img.height * scale), resample=PIL.Image.NEAREST
-            )
-        img = img.convert("RGB").convert("1")
-        if img.width < PRINTER_WIDTH:
-            pad_amount = (PRINTER_WIDTH - img.width) // 2
-            padded_image = PIL.Image.new("1", (PRINTER_WIDTH, img.height), 1)
-            padded_image.paste(img, box=(pad_amount, 0))
-            img = padded_image
-
-        for y in range(0, img.height):
-            bmp = []
-            bit = 0
-
-            # pack image data into 8 pixels per byte
-            for x in range(0, img.width):
-                if bit % 8 == 0:
-                    bmp += [0x00]
-                bmp[int(bit / 8)] >>= 1
-                if not img.getpixel((x, y)):
-                    bmp[int(bit / 8)] |= 0x80
-                else:
-                    bmp[int(bit / 8)] |= 0
-                bit += 1
-
-            cmdqueue += format_message(Command.DrawBitmap, bmp)
-
-        cmdqueue += format_message(Command.SetControlLattice, FinishLattice)
-        return cmdqueue
-
     image = PIL.Image.open(args.filename)
 
     print_data = []
-    print_data = print_data + render_image(image)
-    print_data = print_data + blank_paper(feed_lines)
+    print_data = print_data + cmd_print_image(image)
+    print_data = print_data + cmd_feed_paper(feed_lines)
 
     asyncio.run(connect_and_send(print_data))
 
